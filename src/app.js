e70732e4bac97c12d25e1743a7a55117e6e14fe0
Filipe Pires <liipepires@gmail.com>
2019-03-01 13:48:16 -0300

    ajustes na configuracao do projeto

--- src/app.js
+++ src/app.js
@@ -1,11 +1,16 @@
+require("dotenv").config({
+  path: process.env.NODE_ENV === "test" ? ".env.test" : ".env"
+})
 const express = require("express")
 
 const routes = require("./routes")
 
 const App = express()
 
+//  Middlewares
 App.use(express.json())
 
+//  Routes
 App.use(routes)
 
 module.exports = App
--- src/routes.js
+++ src/routes.js
@@ -1,14 +1,3 @@
 const Router = require("express").Router()
 
-// const authMiddleware = require("./middleware/auth")
-
-Router.get("/login", (req, res) => {
-  return res.status(200).json({ message: "OK" })
-})
-
-// Router.use(authMiddleware)
-// Router.get("/dashboard", (req, res) => {
-//   return res.status(200)
-// })
-
 module.exports = Router
