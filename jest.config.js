7510d80f7e96753bfe8b301c7a0bd9c67a5624b1
Filipe Pires <liipepires@gmail.com>
2019-03-08 15:11:49 -0300

    add coverage on jest

--- jest.config.js
+++ jest.config.js
@@ -18,16 +18,19 @@ module.exports = {
   clearMocks: true,
 
   // Indicates whether the coverage information should be collected while executing the test
-  collectCoverage: false, // trocado para nao cobrir testes ainda <==
+  collectCoverage: true, // trocado para cobrir testes ainda <==
 
   // An array of glob patterns indicating a set of files for which coverage information should be collected
   // collectCoverageFrom: null,
 
   // The directory where Jest should output its coverage files
-  coverageDirectory: "./__tests/coverage",
+  coverageDirectory: "./__tests__/coverage",
 
   // An array of regexp pattern strings used to skip coverage collection
-  coveragePathIgnorePatterns: ["/node_modules/"],
+  coveragePathIgnorePatterns: [
+    "/node_modules/",
+    "src/database/strategies/interfaces"
+  ],
 
   // A list of reporter names that Jest uses when writing coverage reports
   // coverageReporters: [
